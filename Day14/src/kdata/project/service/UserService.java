c7b1349d1e42c926640e1da6dd91baf1a84b5301
ctyvx784 <ctyvx784@gmail.com>
2017-07-17 16:18:09 +0900

    make abstract method

--- Day14/src/kdata/project/service/UserService.java
+++ Day14/src/kdata/project/service/UserService.java
@@ -1,5 +1,9 @@
 package kdata.project.service;
 
-public interface UserService {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public interface UserService {
+	//UserServlet에서 들어오는 요청에 대한 메소드 생성, doGet으로 받는 변수를 받는다.
+	public abstract NextPage execute(HttpServletRequest request, HttpServletResponse response);
 }
