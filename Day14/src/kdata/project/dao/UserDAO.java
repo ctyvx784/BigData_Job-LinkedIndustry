f9d541f84561ce73fa899b91ce7b42df2bd48226
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 09:51:53 +0900

    메소드 베이스 구축

--- Day14/src/kdata/project/dao/UserDAO.java
+++ Day14/src/kdata/project/dao/UserDAO.java
@@ -2,12 +2,23 @@ package kdata.project.dao;
 
 public class UserDAO {
 	// 회원 가입
-	
+	public void insert(){
+		
+	}
 	//회원 리스트
-	
+	public void selectAll(){
+		
+	}
 	//회원 상세정보
-	
+	public void selectById(){
+		
+	}
 	//로그인
-	
+	public void login(){
+		
+	}
 	//회원정보삭제
+	public void delete(){
+		
+	}
 }
