4ecd82c47bc1c4f7d14db38131e2baadea0dd20d
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 14:10:47 +0900

    modified

--- Day14/src/kdata/project/dto/UserDTO.java
+++ Day14/src/kdata/project/dto/UserDTO.java
@@ -8,54 +8,59 @@ Stream에서 객체를 직렬화 시키기 위해 Serializable 인터페이스�
 특이 사항은로는 overriding을 할 메소드는 없다.*/
 
 public class UserDTO implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4226137163490842665L;
 
-	public UserDTO(String id, String name, String filename, Date date){};
-	public UserDTO(String id, String pw, String name, String filename){};
-	
-	private String id,pw,name,fileName;
+	private String id;
+	private String pw;
+	private String name;
+	private String profile;
 	private Date regDate;
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public Date getRegDate() {
-		return regDate;
-	}
-
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
+	
 	public String getPw() {
 		return pw;
 	}
-
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
-
-	public String getName() {
-		return name;
+	public String getId() {
+		return id;
 	}
-
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getName() {
+		return name;
+	}
+	public UserDTO(String id, String name, String profile, Date regDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.profile = profile;
+		this.regDate = regDate;
+	}
+	public UserDTO(String id, String pw, String name, String profile) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.profile = profile;
+	}
+	
 }
+	
+
+	
