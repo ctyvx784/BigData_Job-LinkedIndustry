b236d96984f6fc466ba38030f68da34bc278f5a6
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 11:29:07 +0900

    add

--- Day14/src/kdata/project/service/UserRegisterService.java
+++ Day14/src/kdata/project/service/UserRegisterService.java
@@ -1,9 +1,12 @@
 package kdata.project.service;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kdata.project.dao.UserDAO;
+import kdata.project.dto.UserDTO;
 
 public class UserRegisterService implements UserService {
 
@@ -16,14 +19,32 @@ public class UserRegisterService implements UserService {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 		String name = request.getParameter("name");
+		String fileName = null;
 		//2. DB 처리(DAO 메소드 호출)
 		/*UserDAO dao = UserDAO.getInstance();
 		dao.insert();*/
-		UserDAO.getInstance().insert();
+		UserDTO user = new UserDTO(id, pw, name, fileName);
+		try {
+			UserDAO.getInstance().insert(user);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//3. DB 결과 처리
 		
 		//4. 이동할 다음 페이지 처리
-		return null;
+		NextPage nextPage = new NextPage();
+		int result = 0;
+		//회원가입 성공
+		if(result !=0){
+			nextPage.setPageName("./user/login.jsp");
+			nextPage.setRedriect(true);
+		}
+		//회원가입 실패
+		else{
+			
+		}
+		return nextPage;
 	}
 
 }
