69233ff2edd13c41bc614e9424609c6dbafec8e2
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 10:33:20 +0900

    add field & comment

--- Day14/src/kdata/project/dto/UserDTO.java
+++ Day14/src/kdata/project/dto/UserDTO.java
@@ -1,9 +1,38 @@
 package kdata.project.dto;
 
-public class UserDTO {
+import java.io.Serializable;
+import java.sql.Date;
+
+/*
+Stream에서 객체를 직렬화 시키기 위해 Serializable 인터페이스를 implements 한다.
+특이 사항은로는 overriding을 할 메소드는 없다.*/
+
+public class UserDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4226137163490842665L;
+
 	public UserDTO(){};
 	
-	String id,pw,name;
+	private String id,pw,name,fileName;
+	private Date regDate;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
 
 	public String getId() {
 		return id;
