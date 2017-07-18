c61eaba0170a87b4f0da4ebc5289fccadcc277db
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 16:32:20 +0900

    UserDetailService 구현

--- Day14/src/kdata/project/service/UserDetailService.java
+++ Day14/src/kdata/project/service/UserDetailService.java
@@ -1,14 +1,36 @@
 package kdata.project.service;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kdata.project.dao.UserDAO;
+import kdata.project.dto.UserDTO;
 
 public class UserDetailService implements UserService {
 
 	@Override
 	public NextPage execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-		return null;
+		System.out.println("2.UserDetailService");
+		String id = request.getParameter("id");
+
+		UserDTO user = null;
+		try {
+			user = UserDAO.getInstance().selectById(id);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		user.toString();
+		request.setAttribute("user", user);
+
+		NextPage nextPage = new NextPage();
+
+		nextPage.setPageName("./user/detail.jsp");
+		nextPage.setRedriect(false);
+		return nextPage;
 	}
 
 }
