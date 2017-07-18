35efbe1259e310cbe20abf186c3383df7c5200c1
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 09:47:21 +0900

    요청 파라미터 처리

--- Day14/src/kdata/project/service/UserRegisterService.java
+++ Day14/src/kdata/project/service/UserRegisterService.java
@@ -8,6 +8,16 @@ public class UserRegisterService implements UserService {
 	@Override
 	public NextPage execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
+		//1.요청 파라미터 처리
+		//jsp에서 input의 name에 들어간 값을 가져온다.
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		//2. DB 처리(DAO 메소드 호출)
+		
+		//3. DB 결과 처리
+		
+		//4. 이동할 다음 페이지 처리
 		return null;
 	}
 
