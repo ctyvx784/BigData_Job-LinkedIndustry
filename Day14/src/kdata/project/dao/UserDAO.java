ec52bc4a1bdcffafd61196f51e2d2b5e55cc7901
ctyvx784 <ctyvx784@gmail.com>
2017-07-18 14:19:25 +0900

    modified

--- Day14/src/kdata/project/dao/UserDAO.java
+++ Day14/src/kdata/project/dao/UserDAO.java
@@ -11,77 +11,86 @@ import kdata.project.dto.UserDTO;
 import kdata.project.util.DBUtil;
 
 public class UserDAO {
-	//싱글턴 패턴--------------------------------------
+	// 싱글턴 패턴--------------------------------------
 	private static UserDAO dao = new UserDAO();
-	private UserDAO(){}
-	
-	public static UserDAO getInstance(){
+
+	private UserDAO() {
+	}
+
+	public static UserDAO getInstance() {
 		return dao;
 	}
-	//----------------------------------------------
+
+	// ----------------------------------------------
 	// 회원 가입
-	public int insert(UserDTO user) throws SQLException{
+	public int insert(UserDTO user) throws SQLException {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		int result = 0;
-		
+
 		try {
 			con = DBUtil.getConnection();
-			String sql = "insert into users values(?,?,?,null,deafult)";
-			
+			String sql = "insert into users values(?,?,?,?,default)";
+
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, user.getId());
 			pstmt.setString(2, user.getPw());
 			pstmt.setString(3, user.getName());
-			
-			result=pstmt.executeUpdate();
-			
+			pstmt.setString(4, user.getProfile());
+
+			result = pstmt.executeUpdate();
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally{
+		} finally {
 			DBUtil.close(con, pstmt);
 		}
 		return result;
 	}
-	//회원 리스트
-	public List<UserDTO> selectAll(){
+
+	// 회원 리스트
+	public List<UserDTO> selectAll() throws SQLException {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		List<UserDTO> list = new ArrayList<>();
-		
+
 		try {
 			con = DBUtil.getConnection();
-			String sql = "select * from users";
-			
+			String sql = "select * from users order by id";
+
 			pstmt = con.prepareStatement(sql);
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()){
-				UserDTO user = new UserDTO(rs.getString("id"),
-						rs.getString("name"),
-						rs.getString("profile"),
+
+			while (rs.next()) {
+				UserDTO user = new UserDTO(rs.getString("id"), rs.getString("name"), rs.getString("profile"),
 						rs.getDate("ref_date"));
+				list.add(user);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt, rs);
 		}
-		
+
 		return list;
 	}
-	//회원 상세정보
-	public void selectById(){
-		
+
+	// 회원 상세정보
+	public void selectById() {
+
 	}
-	//로그인
-	public void login(){
-		
+
+	// 로그인
+	public void login() {
+
 	}
-	//회원정보삭제
-	public void delete(){
-		
+
+	// 회원정보삭제
+	public void delete() {
+
 	}
 }
