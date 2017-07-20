e4894e966ba6ce08ce50816dfbe28201faa2d0ab
ctyvx784 <ctyvx784@gmail.com>
2017-07-20 09:39:54 +0900

    create method

--- Day14/src/kdata/project/dao/BoardDAO.java
+++ Day14/src/kdata/project/dao/BoardDAO.java
@@ -5,10 +5,25 @@ public class BoardDAO {
 	private static BoardDAO dao = new BoardDAO();
 
 	private BoardDAO() {
-		}
+	}
 
 	public static BoardDAO getInstance() {
 		return dao;
 	}
 	// ----------------------------------------------
+
+	// 댓글 쓰기
+	public void insertComments() {
+
+	}
+
+	// 댓글 리스트
+	public void selectComments() {
+
+	}
+
+	// 댓글 삭제
+	public void deleteComments() {
+
+	}
 }
