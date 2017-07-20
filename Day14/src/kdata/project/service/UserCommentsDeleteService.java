8043bae163aee4d4e448e47d93da5efcd99b2bbf
ctyvx784 <ctyvx784@gmail.com>
2017-07-20 15:25:13 +0900

    deleteService 구현

--- Day14/src/kdata/project/service/UserCommentsDeleteService.java
+++ Day14/src/kdata/project/service/UserCommentsDeleteService.java
@@ -1,14 +1,30 @@
 package kdata.project.service;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kdata.project.dao.BoardDAO;
 
 public class UserCommentsDeleteService implements UserService {
 
 	@Override
 	public NextPage execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+		System.out.println("UserCommentsDeleteService");
+		String commentNum = request.getParameter("comment_num");
+		int result=0;
+		
+		try {
+			result=BoardDAO.getInstance().deleteComments(Integer.parseInt(commentNum));
+		} catch (NumberFormatException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(result !=0)
+			System.out.println("삭제성공");
 		return null;
 	}
 
 }
+
