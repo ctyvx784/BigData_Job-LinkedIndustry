d8ae2403e486d9691c865875d24db0c5bad6624c
ctyvx784 <ctyvx784@gmail.com>
2017-07-20 12:37:57 +0900

    insert, select 구현

--- Day14/src/kdata/project/dao/BoardDAO.java
+++ Day14/src/kdata/project/dao/BoardDAO.java
@@ -1,5 +1,15 @@
 package kdata.project.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kdata.project.dto.CommentsDTO;
+import kdata.project.util.DBUtil;
+
 public class BoardDAO {
 	// 싱글턴 패턴--------------------------------------
 	private static BoardDAO dao = new BoardDAO();
@@ -13,13 +23,71 @@ public class BoardDAO {
 	// ----------------------------------------------
 
 	// 댓글 쓰기
-	public void insertComments() {
+	public int insertComments(CommentsDTO comment) throws SQLException {
+		System.out.println("3. BoardDAO insert()");
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			//2. DB서버 연결
+			con = DBUtil.getConnection();
+			String sql = "insert into comments values(comments_seq.nextval,?,default,?,?)";
+			
+			// 3. Statement 객체 생성
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, comment.getId());
+			pstmt.setInt(2, comment.getNumber());
+			pstmt.setString(3, comment.getComment());
+			
+			// 4. SQL문 전송
+			result = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		System.out.println("삽입확인:"+result);
+		return result;
 
 	}
 
 	// 댓글 리스트
-	public void selectComments() {
-
+	public List<CommentsDTO> selectComments(int number) throws SQLException {
+		System.out.println("3. BoardDAO selectComments()");
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<CommentsDTO> list = new ArrayList<>();
+		
+		try {
+			con = DBUtil.getConnection();
+			String sql = "select * from comments where num=?";
+			
+			//3. Statement 객체 생성
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, number);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				CommentsDTO comment = new CommentsDTO(
+						rs.getInt("comment_num"),
+						rs.getString("id"),
+						rs.getDate("write_date"),
+						rs.getString("content"),
+						rs.getInt("num"));
+				list.add(comment);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			DBUtil.close(con, pstmt, rs);
+		}
+		System.out.println("리스트확인:"+list);
+		return list;
 	}
 
 	// 댓글 삭제
